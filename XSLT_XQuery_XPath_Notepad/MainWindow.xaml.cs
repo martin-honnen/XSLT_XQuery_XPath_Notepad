25ae5c83bcfd9feecc49415f145b15c0674d5091
Martin Honnen <martin.honnen@gmx.de>
2021-10-13 07:56:39 +0200

    Projektdateien hinzufügen.

---
+++ XSLT_XQuery_XPath_Notepad/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XSLT_XQuery_XPath_Notepad
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoadXmlInput_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void LoadXmlInput_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+        }
+
+        private void LoadJsonInput_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void LoadJsonInput_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ XSLT_XQuery_XPath_Notepad/MyApplicationCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XSLT_XQuery_XPath_Notepad.Commands
+{
+    public class MyApplicationCommands
+    {
+        public static RoutedUICommand LoadXmlInput = new RoutedUICommand("Load XML input file", "LoadXmlInput", typeof(MyApplicationCommands));
+
+        public static RoutedUICommand LoadJsonInput = new RoutedUICommand("Load JSON input file", "LoadJsonInput", typeof(MyApplicationCommands));
+
+        public static RoutedUICommand LoadXsltCode = new RoutedUICommand("Load XSLT file", "LoadXsltCode", typeof(MyApplicationCommands));
+
+        public static RoutedUICommand LoadXQueryCode = new RoutedUICommand("Load XQuery file", "LoadXQueryCode", typeof(MyApplicationCommands));
+    }
+}
